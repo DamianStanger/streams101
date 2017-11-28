f8d388f58f9db6a4b327ab53e0ea5f21dcf574e8
Damian Stanger <damian.stanger@rbi.co.uk>
2017-11-28 17:49:58 +0000

    addressed problem of the hanging object that only completes at the end

--- writeItAsyncPush.js
+++ writeItAsyncPush.js
@@ -7,26 +7,36 @@ function listenForData(readStream) {
     let inProgress = 0;
     let total = 0;
 
+    function runInProcessWatcher() {
+      if (inProgress >= MAX_LIMIT) {
+        if (!readStream.isPaused()) {
+          readStream.pause();
+          console.log(`            ${inProgress}:${total} Write pausing    0`);
+        }
+        if (inProgress <= 1) {
+          console.log(`            ${inProgress}:${total} Write watching   2`);
+          setTimeout(runInProcessWatcher, ASYNC_DELAY);
+        }
+      } else {
+        readStream.resume();
+        console.log(`            ${inProgress}:${total} Write resumed     1`);
+      }
+    }
+
     function callAsync(obj) {
       function recursiveDone() {
         function asyncDone() {
           readStream.resume();
           inProgress--;
           total++;
-          console.log(`------ ${obj.id} - ${inProgress}:${total} Write finished`);
+          console.log(`------ ${obj.id} - ${inProgress}:${total} Write finished, resuming`);
         }
 
-        if (inProgress >= MAX_LIMIT) {
-          readStream.pause();
-          console.log(`       ${obj.id} - ${inProgress}:${total} Write paused   0`);
-          setTimeout(recursiveDone, ASYNC_DELAY);
-        } else {
-          readStream.resume();
-          const DO_WORK_FOR = 100 + (Math.random() * 900);
-          setTimeout(asyncDone, DO_WORK_FOR);
-          inProgress++;
-          console.log(`       ${obj.id} - ${inProgress}:${total} Write resumed   1`);
-        }
+        const DO_WORK_FOR = 100 + (Math.random() * 900);
+        setTimeout(asyncDone, DO_WORK_FOR);
+        inProgress++;
+
+        runInProcessWatcher();
       }
 
       recursiveDone();
