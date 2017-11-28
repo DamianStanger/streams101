7e2c588a2138e7f2a58309b0cb9c9fbddc5544a8
Damian Stanger <damian.stanger@rbi.co.uk>
2017-11-28 17:59:37 +0000

    clean up the code, remove redundant functions

--- writeItAsyncPush.js
+++ writeItAsyncPush.js
@@ -3,47 +3,39 @@ const ASYNC_DELAY = 500;
 
 
 function listenForData(readStream) {
-  const callAsync = function () {
-    let inProgress = 0;
-    let total = 0;
+  let inProgress = 0;
+  let total = 0;
 
-    function runInProcessWatcher() {
-      if (inProgress >= MAX_LIMIT) {
-        if (!readStream.isPaused()) {
-          readStream.pause();
-          console.log(`            ${inProgress}:${total} Write pausing    0`);
-        }
-        if (inProgress <= 1) {
-          console.log(`            ${inProgress}:${total} Write watching   2`);
-          setTimeout(runInProcessWatcher, ASYNC_DELAY);
-        }
-      } else {
-        readStream.resume();
-        console.log(`            ${inProgress}:${total} Write resumed     1`);
+  function runInProcessWatcher() {
+    if (inProgress >= MAX_LIMIT) {
+      if (!readStream.isPaused()) {
+        readStream.pause();
+        console.log(`            ${inProgress}:${total} Write pausing    0`);
       }
+      if (inProgress <= 1) {
+        console.log(`            ${inProgress}:${total} Write watching   2`);
+        setTimeout(runInProcessWatcher, ASYNC_DELAY);
+      }
+    } else {
+      readStream.resume();
+      console.log(`            ${inProgress}:${total} Write resumed     1`);
+    }
+  }
+
+  function callAsync(obj) {
+    function asyncDone() {
+      readStream.resume();
+      inProgress--;
+      total++;
+      console.log(`------ ${obj.id} - ${inProgress}:${total} Write finished   1`);
     }
 
-    function callAsync(obj) {
-      function recursiveDone() {
-        function asyncDone() {
-          readStream.resume();
-          inProgress--;
-          total++;
-          console.log(`------ ${obj.id} - ${inProgress}:${total} Write finished, resuming`);
-        }
+    const DO_WORK_FOR = 100 + (Math.random() * 900);
+    setTimeout(asyncDone, DO_WORK_FOR);
+    inProgress++;
 
-        const DO_WORK_FOR = 100 + (Math.random() * 900);
-        setTimeout(asyncDone, DO_WORK_FOR);
-        inProgress++;
-
-        runInProcessWatcher();
-      }
-
-      recursiveDone();
-    }
-
-    return callAsync;
-  }();
+    runInProcessWatcher();
+  }
 
 
   readStream.on("data", data => {
