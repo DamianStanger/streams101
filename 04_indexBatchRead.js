904fa91be4b305f90e530c513d910149c295c004
Damian Stanger <damian.stanger@rbi.co.uk>
2018-03-08 08:31:49 +0000

    finished indexBatchRead and added log

--- 04_indexBatchRead.js
+++ 04_indexBatchRead.js
@@ -2,7 +2,7 @@ const ReadItBatch = require("./streams/readItBatch");
 const TransformIt = require("./streams/transformIt");
 const WriteItSyncSlow = require("./streams/writeItSyncSlow");
 
-const readItBatch = new ReadItBatch({highWaterMark: 4, maxReads: 40});
+const readItBatch = new ReadItBatch({highWaterMark: 4, maxReads: 20, batchSize: 5});
 const transformIt = new TransformIt({highWaterMark: 4});
 const writeItSyncSlow = new WriteItSyncSlow({highWaterMark: 4});
 
--- streams/readItBatch.js
+++ streams/readItBatch.js
@@ -1,30 +1,34 @@
 const {Readable} = require("stream");
 const {GREEN, NO_COLOR, DARK} = require("../consoleColors");
 
-const MAX = 20;
-const BATCH = 5;
 
-
-class ReadIt extends Readable {
+class ReadItBatch extends Readable {
   constructor(options) {
     options.objectMode = true;
     super(options);
+    this.max = options.maxReads;
+    this.batch = options.batchSize;
     this.index = 0;
     console.log(`${GREEN}++++++++++ READ Start - ${new Date()}${NO_COLOR}`)
   }
 
+  _buildMsg(batchId, index) {
+    return {id: batchId, currentIndex: index};
+  }
+
   _read() {
     this.index++;
+
     console.log(`++ ${this.index} Read `);
 
-    if (this.index > (MAX / BATCH)) {
+    if (this.index > (this.max / this.batch)) {
       this.push(null);
       console.log(`${GREEN}++++++++++ READ End - ${new Date()}${NO_COLOR}`)
 
     } else {
-      for (let i = 1; i <= BATCH; i++) {
+      for (let i = 1; i <= this.batch; i++) {
         const batchId = this.index + (0.01 * i);
-        var pushResult = this.push({id: batchId, currentIndex: this.index});
+        var pushResult = this.push(this._buildMsg(batchId, this.index));
         console.log(`${DARK}++ ${batchId} Read Batch push:${pushResult?1:0}${NO_COLOR}`)
       }
     }
@@ -33,4 +37,4 @@ class ReadIt extends Readable {
 }
 
 
-module.exports = ReadIt;
+module.exports = ReadItBatch;
