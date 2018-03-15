46633c2317ce9eb0006525cc81ff85d34e5a3a51
Damian Stanger <damian.stanger@rbi.co.uk>
2018-03-15 17:37:51 +0000

    amended the run details to be same as blog, added log file

--- 06_indexEventEmitters.js
+++ 06_indexEventEmitters.js
@@ -2,8 +2,8 @@ const ReadIt = require("./streams/readIt");
 const TransformIt = require("./streams/transformIt");
 const writeItEventEmitters = require("./streams/writeItEventEmitters");
 
-const readIt = new ReadIt({highWaterMark: 5, doSlowReads: false, maxReads: 20});
-const transformIt = new TransformIt({highWaterMark: 5});
+const readIt = new ReadIt({highWaterMark: 2, doSlowReads: false, maxReads: 20});
+const transformIt = new TransformIt({highWaterMark: 2});
 
 readIt.pipe(transformIt);
-writeItEventEmitters(transformIt, {"maxConcurrency": 5});
+writeItEventEmitters(transformIt, {"maxConcurrency": 10});
